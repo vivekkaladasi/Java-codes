116e05b5aee0e49f6b30756b441c4a5aa8ad9605
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-19 22:56:59 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,30 @@
+
+
+class firstClass{
+    public static void main(String[] args) {
+        for(int counter=0;counter<=10;counter++){
+            System.out.print(counter+" ");
+        }
+        System.out.println("replacing counter with i");
+
+        for(int j=0;j<=10;j++){
+            System.out.print(j+" ");
+        }
+
+
+       int h=0;
+       while(h<=10){
+        System.out.println(h+" ");
+        h=h+1;
+       } 
+
+       int t=0;
+       do { 
+           System.out.println(t);
+           t++;
+       } while (t<11);
+
+
+    }
+
+}
