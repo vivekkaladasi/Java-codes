56ee8c507d8289555050c6c886f09c4e6dfd8cfe
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-19 16:08:59 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,32 @@
+//java is a typed language
+
+class firstClass {
+    public static void main (String[] args) {
+        
+        //variable
+
+        String names = "vivek kaladasi";
+
+        double price = 25.25;
+        
+        int a =25;
+
+        int b = 10;
+        int sum = a+b;
+        int diff = b-a;
+        int mul = a*b;
+        System.out.println(names);
+        System.out.println(price);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+
+
+
+
+    }
+}
+
+
