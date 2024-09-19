34778809522ffe57151444fa4fad6a16c0c2dc3c
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-19 18:46:06 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+
+class firstClass {
+   public static void main(String[] args) {
+
+    Scanner sc= new Scanner(System.in);
+    
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    
+
+    if(a==b){
+        System.out.println("a=b");
+    }
+    else if (a<b){
+        System.out.println("a is less than b");
+    }
+    else{
+        System.out.println("a is greater than b");
+    }
+
+    }
+   
+    }
+
+
+
