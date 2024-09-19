5c1de0b1c1d0cc4515d78c1d1261bc387412f466
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-19 23:09:06 +0530

    Create SumOfNIntegers.java

---
+++ SumOfNIntegers.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class firstClass{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+        int sum = 0;
+    
+        for(int i=0;i<=n;i++){
+            sum = sum+i;
+        }
+        System.out.print(sum);      
+    
+}
+}
