78c082b3b05aba3b294889b0196ed618277a1416
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-19 15:41:54 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+class firstClass {
+    public static void main (String[] args) {
+        
+
+        //output
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
+
+
