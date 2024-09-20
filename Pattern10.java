bc3196e55a8eb1e0ff3a3565e9377304e81bce84
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-20 17:04:08 +0530

    Create Pattern10.java
    Output:
    *      *
    **    **
    ***  ***
    ********
    ********
    ***  ***
    **    **
    *      *

---
+++ Pattern10.java
@@ -0,0 +1,52 @@
+
+//floyd's triangle
+
+import java.util.*;
+class firstClass{
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //upper half 
+        for(int i=1;i<=n;i++){
+            for( int j=1;j<=i;j++){
+                System.out.print("*");
+  
+            }
+            //spaces
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //2nd part
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for(int i=n;i>=1;i--){
+            for( int j=1;j<=i;j++){
+                System.out.print("*");
+  
+            }
+            //spaces
+            int spaces = 2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //2nd part
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    
+    } 
+}
+
