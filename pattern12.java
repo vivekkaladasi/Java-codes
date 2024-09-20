200609eddf629a0db1e719b6db2c1985b47fcd8a
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-20 18:32:19 +0530

    Create pattern12.java
    output
       1
      2 2
     3 3 3
    4 4 4 4
    5 5 5 5 5

---
+++ pattern12.java
@@ -0,0 +1,31 @@
+//solid rhombus
+
+import java.util.*;
+class firstClass{
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    
+        for(int i=1; i<=n;i++){
+
+            for(int j=1; j<n-i; j++){
+                System.out.print(" ");
+
+            }
+
+            for(int j=1; j<=i ;j++){
+                System.out.print(i + " ");
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    
+} 
+
+
