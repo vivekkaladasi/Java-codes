d1b8b553c7493b67301c0b8e0f6cd4742408ba7e
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-20 17:16:17 +0530

    Create pattern11.java
    output
        *****
       *****
      *****
     *****
    *****

---
+++ pattern11.java
@@ -0,0 +1,28 @@
+//solid rhombus
+
+import java.util.*;
+class firstClass{
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+
+        }
+
+        //stars
+        for(int j=1; j<=n;j++){
+            System.out.print("*");
+        }
+
+        System.out.println();
+
+       }
+
+    
+    } 
+}
+
