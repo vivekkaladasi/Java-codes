51300ab46a191d4572140c90c7d3a00a0b079b54
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-21 18:10:53 +0530

    Create CaluclateSum.java

---
+++ CaluclateSum.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+class firstClass{
+
+    public static int  calculateSum( int a , int b){
+        int sum =a+b;
+        
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+ 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+         int sum = calculateSum(a, b);
+       System.out.println("sum of a + b is : " + sum);
+       
+    
+
+    }
+}
