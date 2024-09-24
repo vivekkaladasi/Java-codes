9e30b644f1c0a2628bd93880b88128ab3a270f02
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-24 18:17:51 +0530

    Create Array2.java

---
+++ Array2.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        
+        int numbers[]=new int[size];
+        //input
+        
+        for(int i=0; i<size;i++){
+            numbers[i]= sc.nextInt();
+        }
+        //output
+
+        for(int i = 0; i<size; i++){
+            System.out.println(numbers[i]);
+        }
+    }
+    
+}
