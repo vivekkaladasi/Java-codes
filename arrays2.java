1eabbae7c0de65c1e2c1974e4f1debb13e3863e1
vivek kaladasi <82016525+vivekkaladasi@users.noreply.github.com>
2024-09-28 17:24:49 +0530

    Create arrays2.java

---
+++ arrays2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Arrays{
+
+public static void main(String[] args) {
+  
+
+  Scanner sc = new Scanner(System.in);
+
+  int size = sc.nextInt();
+
+  int numbers[] = new int[size];
+
+ //input
+  for(int i=0; i<size;i++){
+    numbers[i] = sc.nextInt();
+  }
+
+  int x = sc.nextInt();
+
+  //output
+  //for(int i=0; i<size;i++){
+    //System.out.print(numbers[i]);
+  //}
+  for(int i=0; i<size;i++){
+    if(numbers[i]==x){
+      System.out.println("x found at index: " + i);
+    }
+  }
+
+
+
+
+    
+}
+
+}
